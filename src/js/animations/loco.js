edf6f1d5595ede475a2756e06d161043aa1b5544
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-09-04 16:47:18 +0200

    Added scrollto

--- src/js/animations/loco.js
+++ src/js/animations/loco.js
@@ -4,3 +4,23 @@ export const scroll = new LocomotiveScroll({
   el: document.querySelector('[data-scroll-container]'),
   smooth: true,
 });
+
+const workLink = document.querySelector('#work__link');
+const homeLink = document.querySelector('#home__link');
+const contactLink = document.querySelector('#contact__link');
+const work = document.querySelector('.selected__work');
+const top = document.querySelector('.top');
+const contact = document.querySelector('.contact');
+
+workLink.addEventListener('click', () => {
+  scroll.scrollTo(work);
+});
+
+homeLink.addEventListener('click', () => {
+  scroll.scrollTo(top);
+  console.log('homelinik');
+});
+
+contactLink.addEventListener('click', () => {
+  scroll.scrollTo(contact);
+});
