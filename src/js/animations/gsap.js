5c71fefbacf4d6c656a8af3d0e0ab33e2b7db981
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-09-04 20:19:05 +0200

    changed easing in intro animation

--- src/js/animations/gsap.js
+++ src/js/animations/gsap.js
@@ -2,7 +2,8 @@ import { gsap } from 'gsap';
 
 gsap.from('.smooth-scroll', {
   opacity: 0,
-  duration: 3,
+  duration: 4,
+  ease: 'sine.out',
 });
 
 gsap.from('.header', { opacity: 0, duration: 4 });
