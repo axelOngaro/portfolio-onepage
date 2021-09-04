c1bad829350fac1385d60f7c0987aa0184615802
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-09-04 16:34:10 +0200

    Fixed sticky header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 import { scroll } from './animations/loco';
 
-scroll();
+// scroll
+
+window.addEventListener('load', () => {
+  scroll.update();
+});
+
+setTimeout(() => {
+  scroll.update();
+  console.log('scroll updated');
+}, 5000);
