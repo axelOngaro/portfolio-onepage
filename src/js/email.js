9388224b30984509f36181bd29bc84572faf52d8
Axel Ongaro <axel.ongaro.pro@gmail.com>
2021-09-06 13:03:07 +0200

    Added email form

---
+++ src/js/email.js
@@ -0,0 +1,11 @@
+export const sendMail = () => {
+  const tempParams = {
+    user_name: document.getElementById('user_name').value,
+    user_email: document.getElementById('user_email').value,
+    message: document.getElementById('message').value,
+  };
+
+  emailjs.send('pro_gmail', 'contact_form', tempParams).then((res) => {
+    console.log('email send');
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
 import { scroll } from './animations/loco';
 import { hiddenAnimationArray, learnMoreAnimationArray } from './animations/cssAnimation';
+
+import { sendMail } from './email';
 import './animations/gsap';
 // scroll
 
 window.addEventListener('load', () => {
   scroll.update();
+});
+
+const formSubmit = document.getElementById('form_submit');
+
+formSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  sendMail();
 });
